f08be164abec9e653d6be62a1a191f6fb5072502
Steve Godin <steve.god@gmail.com>
2020-08-06 17:34:23 -0700

    Support bounding tags on a per model basis with acts_as_bounded_*

--- lib/acts_as_taggable_on/tag.rb
+++ lib/acts_as_taggable_on/tag.rb
@@ -6,6 +6,7 @@ module ActsAsTaggableOn
     ### ASSOCIATIONS:
 
     has_many :taggings, dependent: :destroy, class_name: '::ActsAsTaggableOn::Tagging'
+    has_many :tag_bounds, dependent: :destroy, class_name: '::ActsAsTaggableOn::TagBound'
 
     ### VALIDATIONS:
 
